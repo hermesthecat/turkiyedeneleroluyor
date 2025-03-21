c3c7ad9da5ae21bb81dd5ab952ab92e6d0239565
hermesthecat <4947845+hermesthecat@users.noreply.github.com>
2025-03-21 15:02:26 +0300

    next.config.js dosyasında görüntü alanları için izin verilen domain listesi genişletildi. Yeni eklenen domainler arasında haber siteleri ve medya kaynakları yer alıyor.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.sozcu.com.tr', 'i.hurriyet.com.tr', 'icdn.turkiyegazetesi.com.tr', 'im.haberturk.com', 'picsum.photos'],
+    domains: [
+      'i.sozcu.com.tr', 
+      'i.hurriyet.com.tr', 
+      'icdn.turkiyegazetesi.com.tr', 
+      'im.haberturk.com', 
+      'picsum.photos',
+      'foto.haberler.com',
+      'img.posta.com.tr',
+      'imgrosetta.mynet.com.tr',
+      'img.milliyet.com.tr',
+      'iatkv.tmgrup.com.tr',
+      'i4.hurimg.com',
+      'cdnuploads.aa.com.tr',
+      'cdnimages.milliyet.com.tr',
+      'static.daktilo.com',
+      'i01.sozcucdn.com',
+      'static-img.haberler.com',
+      'img-s-msn-com.akamaized.net',
+      'iasbh.tmgrup.com.tr',
+      'images.bursadabugun.com'
+    ],
   },
 }
 
